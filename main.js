dc2487d032c2b4d2b93d00f637c7c227c47ec3c4
renawmontanari <regiangelica@gmail.com>
2024-04-23 19:43:22 -0300

    finalizando-while

--- main.js
+++ main.js
@@ -5,19 +5,14 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
-    // Template String
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; // Template String
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador += 1;
 }
